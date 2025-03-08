24c9117605b22f7d0255d12a27fab01d43d43ea5
lexynovicecoder <didieyealexandra@gmail.com>
2025-03-08 12:01:30 +0000

    removed --id because it is deprecated

--- render-build.sh
+++ render-build.sh
@@ -8,11 +8,11 @@ mkdir -p models/auraface
 cd models/auraface
 
 echo "Downloading models from Google Drive..."
-gdown --id 1_PJVMVhQcg-1l2mAnkRzp0Nzs0AUgFXh -O 1k3d68.onnx
-gdown --id ZgaS65kQ7-2PMByoKAaNZdWEpMn4dsHx -O 2d106det.onnx
-gdown --id 1IqtYMs55jvL39CDmK_AMXTgSk6xuv-sc -O genderage.onnx
-gdown --id 1rz0UKW1-ucpfZUl9PtmpN7FLL95IZ5gS -O glintr100.onnx
-gdown --id 1rz0UKW1-ucpfZUl9PtmpN7FLL95IZ5gS -O scrfd_10g_bnkps.onnx  # Detection model
+gdown 1_PJVMVhQcg-1l2mAnkRzp0Nzs0AUgFXh -O 1k3d68.onnx
+gdown ZgaS65kQ7-2PMByoKAaNZdWEpMn4dsHx -O 2d106det.onnx
+gdown 1IqtYMs55jvL39CDmK_AMXTgSk6xuv-sc -O genderage.onnx
+gdown 1rz0UKW1-ucpfZUl9PtmpN7FLL95IZ5gS -O glintr100.onnx
+gdown 1rz0UKW1-ucpfZUl9PtmpN7FLL95IZ5gS -O scrfd_10g_bnkps.onnx  # Detection model
 
 echo "Checking if models exist..."
 ls -lah
