13c6bf6f8bff4acdaeadfe94b08a9eadf6015b27
lexynovicecoder <didieyealexandra@gmail.com>
2025-03-09 19:07:59 +0000

    made minor changes but reverted it

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, Response, status, Depends, HTTPException
 import uvicorn
 from sqlmodel import Session, create_engine,SQLModel
 from API.database import DATABASE_URL
@@ -7,16 +7,9 @@ from contextlib import asynccontextmanager
 from API.admin_routers import router_admin
 from API.user_routers import router_user
 import uvicorn
-import numpy as np
-import cv2
-from insightface.app import FaceAnalysis
-from huggingface_hub import snapshot_download
-import os
+
 
 engine = create_engine(DATABASE_URL, echo=True)
-
-
-
 
 
 
@@ -37,31 +30,6 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
-# Download and load AuraFace model
-MODEL_DIR = "models/auraface"
-if not os.path.exists(MODEL_DIR):
-    snapshot_download("fal/AuraFace-v1", local_dir=MODEL_DIR)
-
-face_app = FaceAnalysis(
-    name="auraface",
-    providers=["CPUExecutionProvider"],  # Change to "CUDAExecutionProvider" if using GPU
-    root="."
-)
-face_app.prepare(ctx_id=0)
-
-@app.post("/predict/")
-async def predict(file: UploadFile = File(...)):
-    image = np.frombuffer(await file.read(), np.uint8)
-    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-
-    faces = face_app.get(image)
-    if not faces:
-        return {"error": "No face detected"}
-
-    embedding = faces[0].normed_embedding.tolist()
-    return {"embedding": embedding}
-
-# Add your other FastAPI endpoints below
 
 
 @app.get('/')
